dc31fac3b35745ca3d1f3dbb597d542b5a77a059
Livara <livara@gmail.com>
2023-03-02 22:25:51 +0100

    Add event listener and try to fetch the API

--- client/create.js
+++ client/create.js
@@ -1,2 +1,27 @@
 document.querySelector('.ideasForm');
-document.querySelector('.submit')
+
+const submitBtn = document.querySelector('.submit');
+
+submitBtn.addEventListener('click', function(){
+    const titleBox = document.querySelector('#title');
+    const title = titleBox.value;
+    titleBox.value = "";
+
+    const options = {
+        method:'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            title:  // variable with the value you want to send
+        })
+    };
+    
+    (async () => {
+        const response = await fetch('http://localhost:4000/create', options);
+        const data = await response.json()
+        console.log(data)
+    })()
+})
+
+
--- client/script.js
+++ client/script.js
@@ -1,16 +1,31 @@
-const body = document.querySelector('.container')
-const api = ('http://localhost:4000/show-all');
 
-//fetch the data from your server link
-const getIdeas = async (api) => {
+
+document.addEventListener('DOMContentLoaded', function(landingPage){
+    const getIdeas = async () => {
     try {
-        const response = await fetch (api)
-        const data = await response.json();
-        //console.log(data) to check your console
+        const response = await fetch ('http://localhost:4000/show-all')//fetch the data from your server link
+        const data = await response.json(landingPage(data));
+        // console.log(data)
+        //to check your console
         return data;        
     } catch(err) {
         console.log('ERROR');
     }
 }
+    // landingPage();
+})
 
 
+
+
+// const card = document.createElement('p');
+//     card.classList.add("contentBx");
+//     card.innerHTML = data.title.description;
+//     body.appendChild(card);
+
+//function
+function landingPage(data) {
+    const body = document.querySelector('.container');
+    body.innerHTML = data;
+
+}
